63628d813ff1719baed4db0d8da8b62377c8ed7a
Warze <contact@warze.org>
2023-04-04 11:54:46 +0200

    Added extra functionality to word counter

--- templates/wordcounter/main.js
+++ templates/wordcounter/main.js
@@ -2,8 +2,13 @@ function generate() {
     const text = $('inputText').value;
     const words = text.split(' ').filter((a) => a !== '');
     $('countWords').innerText = words.length;
-    $('countChars').innerText = text.length;
+    if ($('countSpaces').checked) {
+        $('countChars').innerText = text.length;
+    } else {
+        $('countChars').innerText = text.replaceAll(' ','').length;
+    }
 }
 
 $('inputText').oninput = generate;
+$('countSpaces').oninput = generate;
 generate();
