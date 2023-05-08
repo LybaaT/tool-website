6d9eaf9319eab8fb137a9b8948bdf121d3f38154
Lyba T <105508273+LybaaT@users.noreply.github.com>
2023-05-08 10:33:47 +0500

    so much so much

--- templates/citationgenerator/main.js
+++ templates/citationgenerator/main.js
@@ -1,6 +1,19 @@
 const form = document.querySelector('#citation-form');
 
+const select = document.querySelector('#source');
+const source = document.querySelector('#url');
+document.getElementById('url').style.display = 'none';
+document.getElementById('url2').style.display = 'none';
 
+select.addEventListener('change', () => {
+  if (select.value === 'webpage') {
+    document.getElementById('url').style.display = 'inline-block';
+    document.getElementById('url2').style.display = 'inline-block';
+} else if (select.value !== 'webpage') {
+    document.getElementById('url').style.display = 'none';
+    document.getElementById('url2').style.display = 'none';
+}
+});
 
 form.addEventListener('submit', event => {
   event.preventDefault();
