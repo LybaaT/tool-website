67dccef68eeda673e00ce88d7e15cc30653142b2
Lyba T <105508273+LybaaT@users.noreply.github.com>
2023-05-10 22:05:27 +0500

    added api key and edited js for longtail keyword generator

--- templates/longtailkeywordgenerator/main.js
+++ templates/longtailkeywordgenerator/main.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
-const API_KEY = 'your-api-key';
-const SEED_KEYWORD = 'your-seed-keyword';
+const API_KEY = 'zff5YtuhQxVRsMuQiRya';
+const SEED_KEYWORD = form.keyword.value;
 
 const url = 'https://api.keyword.io/v1/search';
 
@@ -9,14 +9,14 @@ const params = {
   api_key: API_KEY,
   keyword: SEED_KEYWORD,
   language: 'en',
-  location: 'us',
-  results: 10
+  location: form.location.value,
+  results: form.result.value,
 };
 
 axios.get(url, { params })
   .then(response => {
     const longtailKeywords = response.data;
-    const outputList = document.getElementById('outputList');
+    const outputList = document.getElementById('outputText');
     longtailKeywords.forEach(keyword => {
       const listItem = document.createElement('li');
       listItem.textContent = keyword;
