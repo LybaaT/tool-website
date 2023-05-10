48072aae68e6230dceba7f2a4b7deb715b99f94e
Lyba T <105508273+LybaaT@users.noreply.github.com>
2023-05-10 22:28:18 +0500

    idk what went wrong with my reading (again)

--- templates/longtailkeywordgenerator/main.js
+++ templates/longtailkeywordgenerator/main.js
@@ -1,28 +1,32 @@
-const axios = require('axios');
 
-const API_KEY = 'zff5YtuhQxVRsMuQiRya';
-const SEED_KEYWORD = form.keyword.value;
-
-const url = 'https://api.keyword.io/v1/search';
-
-const params = {
-  api_key: API_KEY,
-  keyword: SEED_KEYWORD,
-  language: 'en',
-  location: form.location.value,
-  results: form.result.value,
-};
-
-axios.get(url, { params })
-  .then(response => {
-    const longtailKeywords = response.data;
-    const outputList = document.getElementById('outputText');
-    longtailKeywords.forEach(keyword => {
-      const listItem = document.createElement('li');
-      listItem.textContent = keyword;
-      outputList.appendChild(listItem);
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+function searchKeywords() {
+    const API_KEY = 'zff5YtuhQxVRsMuQiRya';
+    const SEED_KEYWORD = document.getElementById('keyword').value;
+    const SELECTED_LOCATION = document.getElementById('location').value;
+    const NUM_RESULTS = document.getElementById('result').value;
+  
+    const url = 'https://api.keyword.io/v1/search';
+  
+    const params = {
+      api_key: API_KEY,
+      keyword: SEED_KEYWORD,
+      language: 'en',
+      location: SELECTED_LOCATION,
+      results: NUM_RESULTS,
+    };
+  
+    axios.get(url, { params })
+      .then(response => {
+        const longtailKeywords = response.data;
+        const outputList = document.getElementById('outputText');
+        outputList.innerHTML = ''; // clear previous results
+        longtailKeywords.forEach(keyword => {
+          const listItem = document.createElement('li');
+          listItem.textContent = keyword;
+          outputList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
