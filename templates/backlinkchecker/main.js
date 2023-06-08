1edd43e76f7efe3bc391d9f24a74b5a89bf84dbc
Lyba T <105508273+LybaaT@users.noreply.github.com>
2023-06-08 22:08:54 +0500

    merded thingies and created input boxes

--- templates/backlinkchecker/main.js
+++ templates/backlinkchecker/main.js
@@ -1,6 +1,7 @@
 const outputElement = document.getElementById('backlinkDiv');
 
 function getBackLinks(url) {
+  var url = document.getElementById("input").value;
   fetch('/api/backlinkchecker?url=' + url).then((response) => {
     response.json().then((obj) => {
       outputElement.innerText = 
