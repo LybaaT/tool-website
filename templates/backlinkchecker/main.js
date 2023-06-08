90ce65e489d51aebf77186e0f210dde8763e4b30
Lyba T <105508273+LybaaT@users.noreply.github.com>
2023-06-08 22:44:45 +0500

    just made things look pretty

--- templates/backlinkchecker/main.js
+++ templates/backlinkchecker/main.js
@@ -4,7 +4,7 @@ function getBackLinks(url) {
   var url = document.getElementById("input").value;
   fetch('/api/backlinkchecker?url=' + url).then((response) => {
     response.json().then((obj) => {
-      outputElement.innerText = 
+      outputElement.value = 
           obj.map(([link, text]) => `${link}: "${text}"`).join("\n");
     })
   })
